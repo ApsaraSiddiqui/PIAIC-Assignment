d6b34c1012511a6651795a8624eddcf8059ec61b
Apsara Siddiqui <114807068+ApsaraSiddiqui@users.noreply.github.com>
2024-06-25 16:48:58 +0500

    Add files via upload

---
+++ Ex4/Ex4.js
@@ -0,0 +1,5 @@
+/*Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. 
+Your output should look something like the following, including the quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”*/
+
+console.log(`Grorge C.Lichtenberg once said," It is strange that only extraordinary men make the discoveries, which later appear so easy and simple."`)
